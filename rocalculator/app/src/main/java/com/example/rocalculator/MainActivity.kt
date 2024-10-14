13ea426b643dd33821d6d76d5d26f1c78a736b30
rodrigocastro27 <a23143@alunos.ipca.pt>
2024-10-14 12:19:44 +0100

    getNumber

--- rocalculator/app/src/main/java/com/example/rocalculator/MainActivity.kt
+++ rocalculator/app/src/main/java/com/example/rocalculator/MainActivity.kt
@@ -6,9 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -18,10 +15,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.rocalculator.ui.theme.RocalculatorTheme
 
 
@@ -31,61 +25,76 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RocalculatorTheme() {
-                CreateButton()
+                Calculator()
             }
         }
     }
 }
 
 @Composable
-fun CreateButton() {
-    val display by remember { mutableStateOf("") }
+fun Calculator() {
+    var display by remember { mutableStateOf("") }
+    var firstValue by remember { mutableStateOf("")}
+    var secondValue by remember { mutableStateOf("")}
+    val operator by remember { mutableStateOf("")}
+    val newoperation by remember { mutableStateOf(false)}
 
 
+    fun getNumber(number: String) {
 
-    Column {
 
-        TextField(value = display, onValueChange = {}, readOnly = true, modifier = Modifier.fillMaxWidth())
+        if (operator == null) {
+            firstValue += number
+            display = firstValue
+        } else {
+            secondValue += number
+            display = secondValue
+        }
+    }
+
+    Column () {
+
+        TextField(value = display, onValueChange = {}, readOnly = true)
 
         Row(verticalAlignment = Alignment.CenterVertically) {
 
             Column {
-                Button(onClick = {}, modifier = Modifier.padding(16.dp)) { Text("MRC") }
-                Button(onClick = {}, modifier = Modifier.padding(16.dp)) { Text("√") }
-                Button(onClick = {}, modifier = Modifier.padding(16.dp)) { Text("7") }
-                Button(onClick = { }, modifier = Modifier.padding(16.dp)) { Text("4") }
-                Button(onClick = { }, modifier = Modifier.padding(16.dp)) { Text("1") }
-                Button(onClick = { }, modifier = Modifier.padding(16.dp)) { Text("0") }
+                Button(onClick = {}) { Text("MRC") }
+                Button(onClick = {}) { Text("√") }
+                Button(onClick = {getNumber("7")}) { Text("7") }
+                Button(onClick = {getNumber("4")}) { Text("4") }
+                Button(onClick = {getNumber("1")}) { Text("1") }
+                Button(onClick = {getNumber("0")}) { Text("0") }
             }
 
 
             Column {
-                Button(onClick = {}, modifier = Modifier.padding(16.dp)) { Text("M-") }
-                Button(onClick = {}, modifier = Modifier.padding(16.dp)) { Text("%") }
-                Button(onClick = {}, modifier = Modifier.padding(16.dp)) { Text("8") }
-                Button(onClick = {}, modifier = Modifier.padding(16.dp)) { Text("5") }
-                Button(onClick = {}, modifier = Modifier.padding(16.dp)) { Text("2") }
-                Button(onClick = {}, modifier = Modifier.padding(16.dp)) { Text(".") }
+                Button(onClick = {}) { Text("M-") }
+                Button(onClick = {}) { Text("%") }
+                Button(onClick = {getNumber("8")}) { Text("8") }
+                Button(onClick = {getNumber("5")}) { Text("5") }
+                Button(onClick = {getNumber("2")}) { Text("2") }
+                Button(onClick = {}) { Text(".") }
             }
 
 
             Column {
-                Button(onClick = {}, modifier = Modifier.padding(16.dp)) { Text("M+") }
-                Button(onClick = {}, modifier = Modifier.padding(16.dp)) { Text("+/-") }
-                Button(onClick = {}, modifier = Modifier.padding(16.dp)) { Text("9") }
-                Button(onClick = {}, modifier = Modifier.padding(16.dp)) { Text("6") }
-                Button(onClick = {}, modifier = Modifier.padding(16.dp)) { Text("3") }
-                Button(onClick = {}, modifier = Modifier.padding(16.dp)) { Text("=") }
+                Button(onClick = {}) { Text("M+")}
+                Button(onClick = {}) { Text("+/-") }
+                Button(onClick = {getNumber("9")}) { Text("9") }
+                Button(onClick = {getNumber("6")}) { Text("6") }
+                Button(onClick = {getNumber("3")}) { Text("3") }
+                Button(onClick = {}) { Text("=") }
             }
 
 
             Column {
-                Button(onClick = {}, modifier = Modifier.padding(8.dp)) { Text("ON/C") }
-                Button(onClick = {}, modifier = Modifier.padding(16.dp)) { Text("CE") }
-                Button(onClick = {}, modifier = Modifier.padding(16.dp)) { Text("/") }
-                Button(onClick = {}, modifier = Modifier.padding(16.dp)) { Text("*") }
-                Button(onClick = {}, modifier = Modifier.padding(16.dp)) { Text("-") }
-                Button(onClick = {}, modifier = Modifier.padding(16.dp)) { Text("+") }
+                Button(onClick = {}) { Text("ON/C") }
+                Button(onClick = {}) { Text("CE") }
+                Button(onClick = {}) { Text("/") }
+                Button(onClick = {}) { Text("*") }
+                Button(onClick = {}) { Text("-") }
+                Button(onClick = {}) { Text("+") }
             }
         }
     }
@@ -96,6 +105,6 @@ fun CreateButton() {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    CreateButton()
+fun CalculatorPreview() {
+    Calculator()
 }
